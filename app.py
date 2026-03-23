e09c3bca208135e528467d7c81dea63e5306e1b5
Somya Singhal <somyasinghalup23@email.com>
2026-03-23 20:13:42 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -29,4 +29,8 @@ def Home():
 
     return render_template("index.html", house_value=result)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
